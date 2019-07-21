34c059831d1f58962b8a3c7af6dababa78bfaa68
haozw <517262544@qq.com>
2019-07-21 22:22:20 +0800

    20190721

--- src/main/java/com/hao/cicada/CicadaApplication.java
+++ src/main/java/com/hao/cicada/CicadaApplication.java
@@ -3,6 +3,10 @@ package com.hao.cicada;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author haozw
+ * @date 20190721
+ */
 @SpringBootApplication
 public class CicadaApplication {
 
